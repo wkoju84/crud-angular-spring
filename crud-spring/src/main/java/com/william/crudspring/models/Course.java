d30288ecad359265b8f784f2edcdce58d9d7505a
wkoju84 <wkoju84@gmail.com>
2024-02-12 15:02:34 -0300

    Configurando banco h2

--- crud-spring/src/main/java/com/william/crudspring/models/Course.java
+++ crud-spring/src/main/java/com/william/crudspring/models/Course.java
@@ -1,5 +1,6 @@
 package com.william.crudspring.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,11 +10,12 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @Column(length = 200, nullable = true)
     private String name;
 
-    @Column(length = 10, nullable = false)
+    @Column(length = 10, nullable = true)
     private String category;
 }
